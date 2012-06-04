8c8a9e7b8f01e3034fb3800aa2675985418a5394
Steve Suehs <skelter.net@gmail.com>
2012-06-03 22:53:33 -0500

    Add clutch and congomongo as deps.

--- project.clj
+++ project.clj
@@ -1,3 +1,5 @@
 (defproject couch2mongo "1.0.0-SNAPSHOT"
   :description "Pull dash values from couchdb and push them to a dash document in mongodb"
-  :dependencies [[org.clojure/clojure "1.3.0"]])
+  :dependencies [[org.clojure/clojure "1.3.0"]
+		 [com.ashafa/clutch "0.4.0-SNAPSHOT"]
+		 [congomongo "0.1.9"]])
