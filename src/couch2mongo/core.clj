926bb4fa5a7bc4b767f605bf5606311165b65870
Steve Suehs <skelter.net@gmail.com>
2012-06-03 23:06:06 -0500

    require (as opposed to :use) clutch and congogomongo, but mapped to our names for them.

--- src/couch2mongo/core.clj
+++ src/couch2mongo/core.clj
@@ -1,3 +1,8 @@
 (ns couch2mongo.core
-  (:use [clutch])
+  "Being able to map incoming dependencies to our specific names reminds me of Inferno's mechanisms for mapping
+   external namespaces to process name spaces."
+  (:require [com.ashafa.clutch :as couch]
+	    [somnium.congomongo :as mongo]))
+
+
 
